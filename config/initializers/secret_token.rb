558a6a12802e6f41539c7a4e7360b9a7bbfe226f
Adrian <adrienfrancis.bourgeois@gmail.com>
2013-10-27 22:59:17 +1100

    home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gavroches::Application.config.secret_key_base = '2a7bd097cfc083e9d53a226242c2b831695919072ef2f15cb6956d9404ffa1dc8a824687ad3a5e3630fa1f0be08453a0116cc2005e71994eaeb4fc6663deaf9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gavroches::Application.config.session_store :cookie_store, key: '_gavroches_session'
